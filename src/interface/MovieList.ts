edbcbe79dba998872c7adfe62bf047a6be9da39b
Irfan Mulya <irfanmulya619@gmail.com>
2026-05-04 07:42:27 +0700

    sukses

--- src/interface/MovieList.ts
+++ src/interface/MovieList.ts
@@ -2,5 +2,5 @@ import type DataMovieProps from "./DataMovies";
 
 export default interface MovieListProps {
   movies: DataMovieProps[];
-  onSelectMovie: (id: string | null) => void;
+  onSelectMovie: (id: string | number) => void;
 }
